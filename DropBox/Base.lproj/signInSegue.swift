2489266f3c931053d1c81516f46a566f529770a0
omari1914 <omari.brandt@gmail.com>
2016-05-23 23:20:01 -0700

    Add files via upload

---
+++ DropBox/Base.lproj/signInSegue.swift
@@ -0,0 +1,13 @@
+//
+//  signInSegue.swift
+//  DropBox
+//
+//  Created by Omari Brandt on 5/23/16.
+//  Copyright © 2016 Omari Brandt. All rights reserved.
+//
+
+import Foundation
+
+performSegueWithIdentifier("signInSegue", sender: self)
+
+//performSegueWithIdentifier("createAccountSegue", sender: self)
---
+++ DropBox/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  DropBox
+//
+//  Created by Omari Brandt on 5/22/16.
+//  Copyright © 2016 Omari Brandt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
+
